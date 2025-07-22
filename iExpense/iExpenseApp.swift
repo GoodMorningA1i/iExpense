e31267bbe1d3dc8511d786df653efdb0155849eb
Ali Syed <alisyed0206@gmail.com>
2025-07-21 20:33:59 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ali Syed on 2025-07-21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
